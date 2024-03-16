5aee9e5d02a05ee9028b138e408c991c71533a97
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2024-03-16 15:55:05 +0100

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlflowProject import logger
 from mlflowProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlflowProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlflowProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion"
@@ -22,6 +23,19 @@ try:
         logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<")
         data_validation = DataValidationTrainingPipeline()
         data_validation.main()
+        logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation"
+
+try:
+        logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<")
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
         logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<")
     
 except Exception as e:
---
+++ src/mlflowProject/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,29 @@
+from mlflowProject.config.configuration import ConfigurationManager
+from mlflowProject.components.data_transformation import DataTransformation
+# from mlflowProject import logger
+
+
+# STAGE_NAME = "Data Transformation"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_tranformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.train_test_spliting()
+
+
+# if __name__ == "__main__":
+#     try:
+#         logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<")
+#         obj = DataTransformationTrainingPipeline()
+#         obj.main()
+#         logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<")
+    
+#     except Exception as e:
+#         logger.exception(e)
+#         raise e
+    
