16ecdba1f3b9b0da62754e69e42d5f45f9f10113
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2024-03-16 16:11:12 +0100

    transformation pipeline changes added

--- src/mlflowProject/pipeline/stage_03_data_transformation.py
+++ src/mlflowProject/pipeline/stage_03_data_transformation.py
@@ -1,5 +1,7 @@
 from mlflowProject.config.configuration import ConfigurationManager
 from mlflowProject.components.data_transformation import DataTransformation
+
+from pathlib import Path
 # from mlflowProject import logger
 
 
@@ -7,13 +9,23 @@ from mlflowProject.components.data_transformation import DataTransformation
 
 class DataTransformationTrainingPipeline:
     def __init__(self):
-        pass
+        self.config = ConfigurationManager()
+        self.data_validation_config = self.config.get_data_validation_config()
     
     def main(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_tranformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_spliting()
+        try:
+            with open(Path(self.data_validation_config.STATUS_FILE), 'r') as f:
+                status = f.read().split(" ")[-1]
+                
+            if status:
+                data_transformation_config = self.config.get_data_tranformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+            else:
+                raise Exception("Your data schema is not valid")
+        
+        except Exception as e:
+            print(e)
 
 
 # if __name__ == "__main__":
